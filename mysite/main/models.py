12d5b9002b4af274c4515fa264df67089876edaf
makhmudovMR <mmriadovich@gmail.com>
2019-04-29 13:22:40 +0300

    tutorial model

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Tutorial(models.Model):
+    tutorial_title = models.CharField(max_length=150)
+    tutorial_content = models.TextField(blank=True)
+    tutorial_published = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.tutorial_title
