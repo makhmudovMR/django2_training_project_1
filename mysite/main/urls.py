3812c076fd2428f37723b8ad57405a231dd14175
makhmudovMR <mmriadovich@gmail.com>
2019-04-30 16:08:56 +0300

    login and logout

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -5,5 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', HomePage.as_view(), name='homepage'),
-    path('register/', RegisterPage.as_view(), name='register')
+    path('register/', RegisterPage.as_view(), name='register'),
+    path('logout/', LogoutRequest.as_view(), name='logout'),
+    path('login/', LoginPage.as_view(), name='login')
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 from django.http import HttpResponse
 from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -36,3 +36,29 @@ class RegisterPage(View):
     def get(self, request):
         form = UserCreationForm
         return render(request, 'main/register.html', context={'form': form})
+
+class LogoutRequest(View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, 'Logout successfully')
+        return redirect('main:homepage')
+
+class LoginPage(View):
+
+    def get(self, request):
+        form = AuthenticationForm()
+        return render(request, 'main/login.html', context={'form': form})
+
+    def post(self, request):
+        form = AuthenticationForm(request, data=request.POST) # нужно разобраться с этим
+        print('we are here')
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            print('USER: ',user)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f'Login success, Hello {user.username}')
+                return redirect('main:homepage')
+        return render(request, 'main/login.html', context={'form': form})
